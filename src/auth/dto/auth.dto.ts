065187d60f467ae0ff58aae51686e6a80821cadd
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2024-09-11 14:25:28 +0300

    dto pass change

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -19,35 +19,14 @@ export class AuthDto {
   @IsString({
     message: 'Пароль обязателен',
   })
+  @Matches(
+    new RegExp(
+      '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{6,}$',
+    ),
+    {
+      message:
+        'Пароль должен содержать минимум одну цифру, одну заглавную букву, одну строчную букву и один специальный символ!',
+    },
+  )
   password: string;
 }
-
-// import { IsOptional, IsString, Matches, MinLength } from 'class-validator';
-
-// export class AuthDto {
-//   @IsOptional()
-//   @IsString()
-//   name: string;
-
-//   @IsString({
-//     message: 'Почта обязательна',
-//   })
-//   @Matches(new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$'), {
-//     message: 'Неверный формат почты',
-//   })
-//   email: string;
-
-//   @MinLength(6, {
-//     message: 'Пароль должен содержать не менее 6 символов!',
-//   })
-//   @IsString({
-//     message: 'Пароль обязателен',
-//   })
-//   @Matches(
-//     new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{6,}$'),
-//     {
-//       message: 'Пароль должен содержать минимум одну цифру, одну заглавную букву, одну строчную букву и один специальный символ!',
-//     }
-//   )
-//   password: string;
-// }
